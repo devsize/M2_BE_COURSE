1484b67ac9eadfbfd9c1b0a9bf2201a1974a2d4f
slayer <artrisefrag@gmail.com>
2020-02-18 20:14:24 +0200

    Added ukrainian transletion

--- app/code/Slayer/Test/ViewModel/ExtraInfo.php
+++ app/code/Slayer/Test/ViewModel/ExtraInfo.php
@@ -10,21 +10,21 @@ use Magento\Framework\View\Element\Block\ArgumentInterface;
  */
 class ExtraInfo implements ArgumentInterface
 {
-    const USE_AJAX_LOADING = 'slayer_test/settings/use_ajax';
-
-    /**
-     * @var ScopeConfigInterface
-     */
-    private $scopeConfig;
-
-    /**
-     * @param ScopeConfigInterface $scopeConfig
-     */
-    public function __construct(
-        ScopeConfigInterface $scopeConfig
-    ) {
-        $this->scopeConfig = $scopeConfig;
-    }
+//    const USE_AJAX_LOADING = 'slayer_test/settings/use_ajax';
+//
+//    /**
+//     * @var ScopeConfigInterface
+//     */
+//    private $scopeConfig;
+//
+//    /**
+//     * @param ScopeConfigInterface $scopeConfig
+//     */
+//    public function __construct(
+//        ScopeConfigInterface $scopeConfig
+//    ) {
+//        $this->scopeConfig = $scopeConfig;
+//    }
 
     /**
      * @return string
@@ -42,18 +42,18 @@ class ExtraInfo implements ArgumentInterface
         return $result;
     }
 
-    /**
-     * @return bool
-     */
-    public function useAjax()
-    {
-        $result = false;
-        try {
-            $result = $this->scopeConfig->isSetFlag(self::USE_AJAX_LOADING);
-        } catch (\Exception $exception) {
-            // logger->debug();
-        }
-
-        return $result;
-    }
+//    /**
+//     * @return bool
+//     */
+//    public function useAjax()
+//    {
+//        $result = false;
+//        try {
+//            $result = $this->scopeConfig->isSetFlag(self::USE_AJAX_LOADING);
+//        } catch (\Exception $exception) {
+//             logger->debug();
+//        }
+//
+//        return $result;
+//    }
 }
