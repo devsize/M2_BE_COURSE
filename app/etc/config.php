1e4dcccd9335d9b7171f022af1a21a464ef3590d
slayer <artrisefrag@gmail.com>
2020-04-05 01:45:53 +0200

    commit after reverse

--- app/etc/config.php
+++ app/etc/config.php
@@ -243,6 +243,7 @@ return [
         'Klarna_Kp' => 1,
         'Magento_PaypalReCaptcha' => 1,
         'MSP_TwoFactorAuth' => 1,
+        'Slayer_Mobile' => 1,
         'Slayer_Test' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1,
