4cf442231f6b0d5ca30fe673b346cd2e533c2887
slayer <artrisefrag@gmail.com>
2020-04-07 22:31:38 +0200

    added second store, 2 store view. Created price plagin for phones entity and integreited external currency rate system, witch convert currency depending on store views

---
+++ app/code/Slayer/Mobile/Model/Currency/Import/CurrencyLayer.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Slayer\Mobile\Model\Currency\Import;
+
+use \Magento\Framework\Json\Helper\Data;
+use \Magento\Framework\HTTP\ZendClient;
+use \Magento\Framework\HTTP\ZendClientFactory;
+use \Magento\Framework\App\Config\ScopeConfigInterface;
+use \Magento\Directory\Model\CurrencyFactory;
+
+/**
+ * Currency rate import model (From https://currencylayer.com/)
+ */
+class CurrencyLayer extends \Magento\Directory\Model\Currency\Import\AbstractImport
+{
+    /**
+     * @var string
+     */
+    const CURRENCY_CONVERTER_URL = 'http://apilayer.net/api/live?access_key={{API_KEY}}&currencies={{CURRENCY_TO}}
+                                    &source={{CURRENCY_FROM}}&format=1';
+
+    /** @var Data */
+    protected $jsonHelper;
+
+    /**
+     * Http Client Factory
+     * @var ZendClientFactory
+     */
+    protected $httpClientFactory;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @param CurrencyFactory $currencyFactory
+     * @param ScopeConfigInterface $scopeConfig
+     * @param ZendClientFactory $httpClientFactory
+     * @param Data $jsonHelper
+     */
+    public function __construct(
+        CurrencyFactory $currencyFactory,
+        ScopeConfigInterface $scopeConfig,
+        ZendClientFactory $httpClientFactory,
+        Data $jsonHelper
+    ) {
+        parent::__construct($currencyFactory);
+        $this->scopeConfig = $scopeConfig;
+        $this->httpClientFactory = $httpClientFactory;
+        $this->jsonHelper = $jsonHelper;
+    }
+
+    /**
+     * @param string $currencyFrom
+     * @param string $currencyTo
+     * @param int $retry
+     * @return float|null
+     */
+    protected function _convert($currencyFrom, $currencyTo, $retry = 0)
+    {
+        $result = null;
+        $timeout = (int)$this->scopeConfig->getValue(
+            'currency/currencyLayer/timeout',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+        $apiKey = $this->scopeConfig->getValue(
+            'currency/currencyLayer/apikey',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+        $url = str_replace('{{CURRENCY_FROM}}', $currencyFrom, self::CURRENCY_CONVERTER_URL);
+        $url = str_replace('{{CURRENCY_TO}}', $currencyTo, $url);
+        $url = str_replace('{{API_KEY}}', $apiKey, $url);
+
+        /** @var ZendClient $httpClient */
+        $httpClient = $this->httpClientFactory->create();
+
+        try {
+            $response = $httpClient->setUri($url)
+                ->setConfig(['timeout' => $timeout])
+                ->request('GET')
+                ->getBody();
+
+            $resultKey = $currencyFrom . $currencyTo;
+            $data = $this->jsonHelper->jsonDecode($response);
+
+            if (isset($data['quotes'][$resultKey])) {
+                $result = (float)$data['quotes'][$resultKey];
+            } else {
+                $this->_messages[] = __('We can\'t retrieve a rate from %1.', $url);
+            }
+        } catch (\Exception $e) {
+            if ($retry == 0) {
+                $this->_convert($currencyFrom, $currencyTo, 1);
+            } else {
+                $this->_messages[] = __('We can\'t retrieve a rate from %1.', $url);
+            }
+        }
+        return $result;
+    }
+}
---
+++ app/code/Slayer/Mobile/Plugin/Model/PhonePricePlugin.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Slayer\Mobile\Plugin\Model;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Directory\Model\Currency;
+use Magento\Directory\Model\CurrencyFactory;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Slayer\Mobile\Api\Data\PhoneInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Backend\Block\Template\Context;
+
+class PhonePricePlugin extends Template
+{
+
+    protected $_storeManager;
+    protected $_currency;
+
+    /**
+     * @var CurrencyFactory
+     */
+    protected $currencyFactory;
+
+    /**
+     *
+     * @param Context $context
+     * @param Currency $currency
+     * @param CurrencyFactory $currencyFactory
+     * @param StoreManagerInterface $storeManager
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Currency $currency,
+        CurrencyFactory $currencyFactory,
+        StoreManagerInterface $storeManager,
+        array $data = []
+    ) {
+        $this->_currency = $currency;
+        $this->_storeManager = $storeManager;
+        $this->currencyFactory = $currencyFactory;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @param $amountValue
+     * @return float|int
+     * @throws NoSuchEntityException
+     */
+    private function convertPrice($amountValue)
+    {
+        $currentCurrency = $this->_storeManager->getStore()->getCurrentCurrencyCode();
+        $baseCurrency = $this->_storeManager->getStore()->getBaseCurrency()->getCode();
+        try {
+            if (($currentCurrency && $baseCurrency) != null) {
+                if ($currentCurrency != $baseCurrency) {
+                    $rate = $this->_storeManager->getStore()->getCurrentCurrencyRate();
+                    $amountValue = $amountValue * $rate;
+                }
+            }
+        } catch (\Exception $exception) {
+            $error = $exception->getMessage();
+            $text = 'Error happened during currency loading."%s"';
+            $message = sprintf($text, $error);
+            $this->_logger->debug($message);
+        }
+        return $amountValue;
+    }
+
+    /**
+     * @param PhoneInterface $subject
+     * @param $result
+     */
+    public function afterGetPrice(PhoneInterface $subject, $result)
+    {
+        try {
+            if (is_numeric($result)) {
+                $result = round($this->convertPrice($result), 2);
+                $currencySymbol = $this->_currency->getCurrencySymbol();
+            }
+        } catch (\Exception $exception) {
+            $exception->getMessage();
+        }
+        echo "$result <b class=\"currency\"> &nbsp;$currencySymbol</b>";
+    }
+}
